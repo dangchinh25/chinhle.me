5b221bad2e04e4bc2876f48a5b833a6d5f14c908
dangchinh25 <dangchinh2501@gmail.com>
2023-07-05 11:52:57 -0500

    Update social icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,41 @@
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import {Grid, IconButton} from '@mui/material';
+import Link from './Link';
+import Image from 'next/image';
+
 export default function Footer() {
     return (
         <div>
-            <div
-                style={{
-                display: "block",
-                height: "1px",
-                border: 0,
-                borderTop: "1px solid #ccc",
-                margin: "1em 0",
-                padding: 0,
-                }}
-            ></div>
-            <div>
-                <a href="https://github.com/dangchinh25">Github</a>
-                <a href="https://www.youtube.com/channel/UCu5VfUF_Tdv9EJP5YORfMzg">Youtube</a>
-                <a href="https://www.instagram.com/dangg_chinh/">Instagram</a>
-            </div>
+            <SlimSolidLine />
+
+            <Grid>
+                <Link href="https://github.com/dangchinh25">
+                    <IconButton size='large'>
+                        <GitHubIcon />
+                    </IconButton>
+                </Link>
+                <Link href="https://www.youtube.com/channel/UCu5VfUF_Tdv9EJP5YORfMzg">
+                    <IconButton
+                        size='large'
+                    >
+                        <YouTubeIcon />
+                    </IconButton>
+                </Link>
+                <Link href="https://www.linkedin.com/in/chinh-d-le/">
+                    <IconButton size='large'>
+                        <LinkedInIcon />
+                    </IconButton>
+                </Link>
+                <Link href="https://www.instagram.com/dangg_chinh/">
+                    <IconButton size='large'>
+                        <InstagramIcon />
+                    </IconButton>
+                </Link>
+            </Grid>
+            
             <div
                 style={{
                 width: "5%",
@@ -25,9 +45,28 @@ export default function Footer() {
                 }}
             >
                 <a href="https://github.com/dangchinh25">
-                    <img src="./vercel.svg" />
+                    <Image 
+                        src="/Subject.png"
+                        alt='memoji'
+                        width='100'
+                        height='75'/>
                 </a>
             </div>
         </div>
     )
+}
+
+function SlimSolidLine() {
+    return (
+        <div
+            style={{
+            display: "block",
+            height: "1px",
+            border: 0,
+            borderTop: "1px solid #ccc",
+            margin: "1em 0",
+            padding: 0,
+            }}
+        ></div>
+)
 }
