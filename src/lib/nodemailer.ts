b2f071d0b9f9b9d2ee5dcd2521836f15e0254f51
dangchinh25 <dangchinh2501@gmail.com>
2023-12-23 19:57:46 -0600

    Add lib to send mail from nodemailer

---
+++ src/lib/nodemailer.ts
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+const transport = nodemailer.createTransport({
+  service: 'gmail'
+  , auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.GOOGLE_APP_PASSWORD,
+  },
+});
+
+export type SendMailParams = {
+  text: string
+  fromEmail: string
+  fromName: string
+};
+
+export const sendMail = async (params: SendMailParams) => {
+  const mailOptions: Mail.Options = {
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    // cc: email, (uncomment this line if you want to send a copy to the sender)
+    subject: `Message from ${params.fromName} (${params.fromEmail})`,
+    text: params.text,
+  };
+
+  const sendMailResult = await transport.sendMail(mailOptions);
+
+  return sendMailResult;
+};
