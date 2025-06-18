7b84ca68d13433475b21a51ef1efbe37a3bfe366
dangchinh25 <dangchinh2501@gmail.com>
2025-06-18 12:02:57 -0700

    editor theme

--- src/components/CodeEditor/MarkdownCodeEditor.tsx
+++ src/components/CodeEditor/MarkdownCodeEditor.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import { useMemo } from "react";
+import type { ComponentProps, FC } from "react";
+import { useMemo, useRef } from "react";
 
+import { autocompletion } from "@codemirror/autocomplete";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
+import type { Extension } from "@uiw/react-codemirror";
 import CodeMirror, { EditorView } from "@uiw/react-codemirror";
 
-export const MarkdownCodeEditor = () => {
-    const staticMarkdownExtensions = useMemo(() => {
+import { editorTheme } from "./editorTheme";
+
+export type MarkdownCodeEditorProps = ComponentProps<typeof CodeMirror>;
+
+export const MarkdownCodeEditor: FC<MarkdownCodeEditorProps> = (props) => {
+    const editorViewRef = useRef<EditorView | null>(null);
+    const { ...codeMirrorProps } = props;
+
+    const staticMarkdownExtensions = useMemo<Extension[]>(() => {
         return [
             EditorView.lineWrapping,
             markdown({
@@ -17,13 +27,39 @@ export const MarkdownCodeEditor = () => {
         ];
     }, []);
 
-    const markdownExtensions = useMemo(() => {
-        return [...staticMarkdownExtensions];
-    }, [staticMarkdownExtensions]);
+    const autocompletionExtensions = useMemo<Extension[]>(() => {
+        return [
+            autocompletion({
+                activateOnTyping: true,
+                closeOnBlur: false,
+            }),
+        ];
+    }, []);
+
+    const extensions = useMemo(
+        () => [...staticMarkdownExtensions, ...autocompletionExtensions],
+        [staticMarkdownExtensions, autocompletionExtensions],
+    );
+
+    const onClickHandle = () => {
+        if (editorViewRef.current) {
+            editorViewRef.current.focus();
+        }
+    };
 
     return (
-        <div className="border rounded-md">
-            <CodeMirror height="200px" extensions={markdownExtensions} />
-        </div>
+        <CodeMirror
+            theme={editorTheme}
+            extensions={extensions}
+            basicSetup={{
+                lineNumbers: false,
+                foldGutter: false,
+            }}
+            onCreateEditor={(view) => {
+                editorViewRef.current = view;
+            }}
+            onClick={onClickHandle}
+            {...codeMirrorProps}
+        />
     );
 };
