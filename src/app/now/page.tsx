49cd87ee0b90d4a7779839fbe422b8cb4f0e5fa2
dangchinh25 <dangchinh2501@gmail.com>
2025-06-17 19:38:31 -0700

    update now page

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -5,15 +5,9 @@ export default function NowPage() {
   return (
     <div>
       <TextEntry>
-        Scaling Microservices at Veryable at Core Platform team in Dallas, TX.
+        Buidling AI Agents at <InlineLink to='https://retool.com' displayText='Retool'/>.
       </TextEntry>
-      <TextEntry>
-        Trying to finish my backlog of <InlineLink to='/projects' displayText='ideas'/> and learn Rust.
-      </TextEntry>
-      <TextEntry>
-        Digesting <InlineLink to='https://www.oreilly.com/library/view/building-microservices-2nd/9781492034018/' displayText='Building Microservices' /> 
-        <span> </span>and <InlineLink to='https://www.oreilly.com/library/view/the-staff-engineers/9781098118723/' displayText='Staff Engineer Path' />.
-      </TextEntry>
+    
     </div>
   );
 }
