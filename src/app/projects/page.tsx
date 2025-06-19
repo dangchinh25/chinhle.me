c457eb1560691ccd94c9991bb01cb97cd4329d53
dangchinh25 <dangchinh2501@gmail.com>
2025-06-18 21:09:51 -0700

    add project content

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,14 @@ import { MarkdownCodeEditor } from "@/components/CodeEditor/MarkdownCodeEditor";
 import { Sortable } from "@/components/Sortable/Sortable";
 
 const ProjectsPage = () => {
-    return <Sortable items={[<MarkdownCodeEditor value="TBD" />]} />;
+    return (
+        <Sortable
+            items={[
+                <MarkdownCodeEditor value="**Retool Agent**: [Launch Blog Post](https://retool.com/blog/retool-automates-100-million-hours-of-work-launching-agents)" />,
+                <MarkdownCodeEditor value="**Leetcode Proficiency Tracker**: [Github](https://github.com/dangchinh25/leetcode-review-dashboard)" />,
+            ]}
+        />
+    );
 };
 
 export default ProjectsPage;
